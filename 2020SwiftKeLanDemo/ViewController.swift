7be00e2ce6c805de3f28fbc08f7d328b0ab4de2f
ASSASSINYM <darlingiloveu@sina.com>
2020-11-06 14:15:58 +0800

    first-commit

---
+++ 2020SwiftKeLanDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2020SwiftKeLanDemo
+//
+//  Created by ASSASSIN on 2020/11/6.
+//  Copyright © 2020 scbdh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 2020SwiftKeLanDemoTests/_020SwiftKeLanDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  _020SwiftKeLanDemoTests.swift
+//  2020SwiftKeLanDemoTests
+//
+//  Created by ASSASSIN on 2020/11/6.
+//  Copyright © 2020 scbdh. All rights reserved.
+//
+
+import XCTest
+@testable import _020SwiftKeLanDemo
+
+class _020SwiftKeLanDemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
