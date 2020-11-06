d89305162ffa464f09ace5264e01e4f1ddb44387
ASSASSINYM <darlingiloveu@sina.com>
2020-11-06 15:01:52 +0800

    fuck it

---
+++ 2020SwiftKeLanDemo/View/CustomView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomView.swift
+//  2020SwiftKeLanDemo
+//
+//  Created by ASSASSIN on 2020/11/6.
+//  Copyright © 2020 scbdh. All rights reserved.
+//
+
+import UIKit
+
+class CustomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
